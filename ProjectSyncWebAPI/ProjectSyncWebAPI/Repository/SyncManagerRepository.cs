de9236a0a67fe78273135a53f5ebfb5fa8e5d19d
ajay <ajay@AJAY-PC>
2018-10-18 17:49:16 +0530

    Added Controller for Getting pages

---
+++ ProjectSyncWebAPI/ProjectSyncWebAPI/Repository/SyncManagerRepository.cs
@@ -0,0 +1,48 @@
+﻿using ProjectSyncWebAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace ProjectSyncWebAPI.Repository
+{
+    public class SyncManagerRepository
+    {
+        public List<PageViewModel> GetPages()
+        {
+            List<PageViewModel> pageViewModel = new List<PageViewModel>();
+            string query = "Select PageID,PageName from Pages";
+            SqlConnection conn = new SqlConnection();
+
+            try
+            {
+                conn.ConnectionString = @"Data Source=AJAY-PC\SQLEXPRESS;Initial Catalog=Practice;Integrated Security=True";
+                conn.Open();
+                SqlCommand cmd = new SqlCommand(query, conn);
+                SqlDataReader reader = cmd.ExecuteReader();
+                
+                DataTable data = new DataTable();
+                data.Load(reader);
+
+                foreach(DataRow row in data.Rows)
+                {
+                    pageViewModel.Add(new PageViewModel()
+                    {
+                        PageID = row["PageID"] != null ? Convert.ToInt32(row["PageID"]) : -1,
+                        PageName = row["PageName"] != null? Convert.ToString(row["PageName"]):string.Empty
+                    });
+                }
+
+                conn.Close();
+                return pageViewModel;
+            }
+            catch(Exception ex)
+            {
+                conn.Close();
+                throw ex;
+            }
+        }
+    }
+}
